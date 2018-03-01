83f9dc04123ece286e0fbfe3db93177004482ff4
Jessica Kerr <jessitron@gmail.com>
2018-02-28 20:19:33 -0600

    Initial commit from Atomist

---
+++ src/main/java/com/boinger/jessitron/BoingyboingyApplication.java
@@ -0,0 +1,12 @@
+package com.boinger.jessitron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoingyboingyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoingyboingyApplication.class, args);
+	}
+}
